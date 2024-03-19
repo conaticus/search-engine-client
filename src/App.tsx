b022682b765f0ea71ea804bb3aa2059d5fb12a61
conaticus <enquiry.conaticus@gmail.com>
2024-03-19 19:22:41 +0000

    request api & get results

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import { useState } from "react";
 import Input from "./ui/Input";
 
+interface QueryResult {
+    executionSeconds?: number;
+    results: string[];
+}
+
 function App() {
     const [input, setInput] = useState("");
-    const [results, setResults] = useState<string[]>([]);
+    const [queryResult, setQueryResult] = useState<QueryResult>({
+        results: [],
+    });
+
+    async function sendQuery() {
+        const res = await fetch("/api/query", {
+            method: "POST",
+            body: JSON.stringify({
+                query: input,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        const data = await res.json();
+        setQueryResult(data);
+    }
 
     return (
         <>
@@ -11,10 +33,18 @@ function App() {
                 placeholder="Lasagne Recipe"
                 input={input}
                 onChange={(e) => setInput(e.target.value)}
+                onSubmit={sendQuery}
             />
 
+            {queryResult.executionSeconds && (
+                <p style={{ color: "#9A9A9A", fontSize: "15px", marginBottom: "20px" }}>
+                    Found {queryResult.results.length} Results ({queryResult.executionSeconds}{" "}
+                    seconds)
+                </p>
+            )}
+
             <div>
-                {results.map((result, idx) => (
+                {queryResult.results.map((result, idx) => (
                     <div style={{ marginBottom: "10px", fontSize: "25px" }}>
                         <a key={idx} href={result}>
                             {result}
--- src/ui/Input.tsx
+++ src/ui/Input.tsx
@@ -2,10 +2,18 @@ import { ChangeEventHandler } from "react";
 
 type Props = {
     onChange: ChangeEventHandler<HTMLInputElement>;
+    onSubmit: Function;
     input: string;
     placeholder: string;
 };
 
-export default function Input({ onChange, input, placeholder }: Props) {
-    return <input placeholder={placeholder} onChange={onChange} value={input} />;
+export default function Input({ onChange, onSubmit, input, placeholder }: Props) {
+    return (
+        <input
+            placeholder={placeholder}
+            onChange={onChange}
+            value={input}
+            onKeyDown={(e) => (e.key == "Enter" ? onSubmit() : null)}
+        />
+    );
 }
